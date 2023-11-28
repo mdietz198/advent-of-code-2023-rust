858693df0b36b39effdad15b43596ea9ea27346a
Mark Dietz <mark.d.dietz@gmail.com>
2023-11-28 09:11:52 -0600

    Pre-reqs setup to read an input file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# advent-of-code-2023-rust
+Code for https://adventofcode.com/2023
+========
+
+Each day is its own executable. Usage is:
+```
+cargo run -- <path-to-input-file>
+```
---
+++ day1/src/main.rs
@@ -0,0 +1,11 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input = fs::read_to_string(&args[1]).expect("Should have been able to read the file");
+    let lines: Vec<&str> = input.split(",").collect();
+    for line in lines {
+        println!("{}", line)
+    }
+}
