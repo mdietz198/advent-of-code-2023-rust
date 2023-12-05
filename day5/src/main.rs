0bf4a13a9c97c51c476dc18739bc9124c9539f69
Mark Dietz <mark.d.dietz@gmail.com>
2023-12-05 09:10:36 -0600

    Day 5 part 2 complete

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
 use std::fs;
-use std::collections::HashMap;
 
 //549922357
 //
@@ -13,8 +12,8 @@ fn main() {
 
     let part1_result = part1(&lines);
     print!("Part 1: {part1_result}\n");
-    //let part2_result = part2(&lines);
-    //print!("Part 2: {part2_result}\n");
+    let part2_result = part2(&lines);
+    print!("Part 2: {part2_result}\n");
 }
 
 struct Mapping {
@@ -58,6 +57,48 @@ fn part1(lines: &Vec<&str>) -> i64 {
     let location = seeds.iter().map(|&s| {
         // put seed through each map
         let mut index = s;
+        for mapping in &mappings {
+            index = mapping.map(index);
+        }
+        index
+    });
+
+    location.min().unwrap()
+}
+
+fn part2(lines: &Vec<&str>) -> i64 {
+    let mut mappings: Vec<Mapping> = Vec::new();
+    let mut seeds: Vec<(i64, i64)> = Vec::new();
+    let mut tuples: Vec<(i64, i64, i64)> = Vec::new();
+    for l in lines {
+        if l.starts_with("seeds") {
+            let v: Vec<i64> = l.split(":").nth(1).unwrap().split(" ").filter(|n| !n.is_empty()).map(|n| n.parse::<i64>().unwrap()).collect();
+            v.chunks(2).for_each(|chunk| seeds.push((chunk[0], chunk[1])));
+            print!("seeds: {seeds:?}\n");
+        } else if l.contains(":") && !tuples.is_empty() {
+            let m = Mapping { sub_map: tuples.clone() };
+            mappings.push(m);
+            tuples.clear();
+        } else if l.chars().next().map(|c| c.is_digit(10)).unwrap_or(false) {
+            let mut i = l.split(" ").filter(|n| !n.is_empty()).map(|n| n.parse::<i64>().unwrap());
+            let tuple = (i.next().unwrap(), i.next().unwrap(), i.next().unwrap());
+            tuples.push(tuple);
+        } else {
+            // Do nothing but skip
+        }
+    }
+    let m = Mapping { sub_map: tuples.clone() };
+    mappings.push(m);
+    tuples.clear();
+
+    let location = seeds.iter().inspect(|s| {
+        let start = s.0;
+        let end = s.0 + s.1;
+        print!("start: {start}, end: {end}\n")
+    }).flat_map(|p| p.0 .. p.0+p.1).map(|s| {
+        // put seed through each map
+        let mut index = s;
+        if index % 10000000 == 0 { print!("{index}\n"); }
         for mapping in &mappings {
             index = mapping.map(index);
         }
